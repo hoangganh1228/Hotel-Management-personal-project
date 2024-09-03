c935224e6bdcd1d68924a67dc5b6ebe3cf178734
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-03 15:53:50 +0700

    Cấu trúc lại folder PUG

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 const database = require("./config/database");
 const bodyParser = require("body-parser");
 
@@ -7,9 +6,12 @@ require("dotenv").config();
 
 const route = require("./routes/client/index.route")
 
+const app = express();
 database.connect();
 
 const port = process.env.PORT;
+
+app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
