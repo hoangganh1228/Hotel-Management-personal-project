84e3498d06cf795fa86489fa31b90801792544c9
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-17 21:28:17 +0700

    1. Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/room-category.controller.js
+++ controllers/admin/room-category.controller.js
@@ -71,7 +71,6 @@ module.exports.create = async (req, res) => {
   })
 }   
 
-
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
   if(req.body.position == "") {
@@ -85,3 +84,43 @@ module.exports.createPost = async (req, res) => {
   res.redirect(`${systemConfig.prefixAdmin}/rooms-category`)
 
 }  
+
+// [GET] /admin/rooms-category/edit/:id
+module.exports.edit = async (req, res) => {
+  
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const data = await RoomCategory.findOne({
+      _id: id,
+      deleted: false
+    })
+
+
+    const records = await RoomCategory.find({
+      deleted: false
+    });
+
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/rooms-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/rooms-category`); 
+  }
+}
+
+// [PATCH] /admin/rooms-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  req.body.position = parseInt(req.body.position)
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  res.redirect("back")
+}
+
