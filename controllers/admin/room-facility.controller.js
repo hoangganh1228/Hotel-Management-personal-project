24cc974312b0eb6345bc9c8752938def22e4e8c2
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-20 09:10:50 +0700

    1. Phần thêm mới cơ sở vật chất

--- controllers/admin/room-facility.controller.js
+++ controllers/admin/room-facility.controller.js
@@ -16,3 +16,30 @@ module.exports.index = async (req, res) => {
     // pagination: objectPagination
   })
 }
+
+// [GET] /admin/rooms-facility/create
+module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false
+  };
+
+
+  res.render("admin/pages/rooms-facility/create", {
+    pageTitle: "Tạo cơ sở vật chất"
+  })
+}
+
+// [POST] /admin/rooms-facility/createPost
+module.exports.createPost = async (req, res) => {
+  if(req.body.position == "") {
+    const count = await RoomFacility.countDocuments();
+    req.body.position = count + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+  
+  const facility = new RoomFacility(req.body);
+  await facility.save();
+  
+  res.redirect(`${systemConfig.prefixAdmin}/rooms-facility`);
+}
--- routes/admin/room-facility.route.js
+++ routes/admin/room-facility.route.js
@@ -5,9 +5,21 @@ const router = express.Router();
 const upload = multer();
 const controller = require("../../controllers/admin/room-facility.controller");
 
-// const validate = require("../../validates/admin/room-category.validate");
+const validate = require("../../validates/admin/room-facility.validate");
 const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single('icon'),
+  uploadCloud.upload,
+  validate.creatPost,   
+  controller.createPost,
+  controller.createPost
+);
+
 
 module.exports = router
