29b961b8c8c714fe2d894ce3a7d8b2258efbe683
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-27 08:34:33 +0700

    Fix phân quyền bên backend

--- controllers/admin/room-category.controller.js
+++ controllers/admin/room-category.controller.js
@@ -71,16 +71,22 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-  
-  if(req.body.position == "") {
-    const count = await RoomCategory.countDocuments();
-    req.body.position = count + 1;
+  const permissions = res.locals.role.permissions
+  if(permissions.includes("products-category_create")) {
+    if(req.body.position == "") {
+      const count = await RoomCategory.countDocuments();
+      req.body.position = count + 1;
+    } else {
+      req.body.position = parseInt(req.body.position)
+    } 
+    const record = new RoomCategory(req.body);
+    await record.save();
+    res.redirect(`${systemConfig.prefixAdmin}/rooms-category`)
   } else {
-    req.body.position = parseInt(req.body.position)
-  } 
-  const record = new RoomCategory(req.body);
-  await record.save();
-  res.redirect(`${systemConfig.prefixAdmin}/rooms-category`)
+    res.send("403");
+    return; 
+  }
+
 
 }  
 
