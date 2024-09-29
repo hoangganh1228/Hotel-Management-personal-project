0e52afd73cb0e48e27e78cfba59985433eec7145
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-29 22:34:52 +0700

    3. Giao diện trang chủ bên client (P3)

--- controllers/client/room.controller.js
+++ controllers/client/room.controller.js
@@ -1,8 +1,39 @@
 const Room = require("../../models/room.model")
+const RoomFacility = require("../../models/room-facility.model")
+const RoomCategory = require("../../models/room-category.model");
 
 // [GET] products/index
 module.exports.index = async (req, res) => {
+  const find = {
+    deleted: false,
+    status: 'active'
+  }
+
+  const sort = {
+    position: -1
+  }
+
+  const rooms = await Room.find(find).sort(sort);
+  const facilities = await RoomFacility.find(find);
+  for(const room of rooms) {
+    room.facilities = [];
+    // console.log(room.room_facilities_id);
+    
+    for(let i = 0; i < room.room_facilities_id.length; i++) {
+      const id = room.room_facilities_id[i];
+      // console.log(id);
+      
+      const facility = await RoomFacility.findOne({
+        _id: id
+      }).select("title")
+      
+      room.facilities.push(facility);
+      
+    }
+  }
   res.render("client/pages/rooms/index", {
     pageTitle: "Trang phòng",
+    rooms: rooms,
+    facilities: facilities
   })
 }
