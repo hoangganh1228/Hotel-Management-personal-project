b0faa9083cd48ca6fd3eaa1f82bdf5d3d3751153
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-10-08 16:40:48 +0700

    3. Đăng nhập tài khoản

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,8 +2,11 @@ const homeRoute = require("./home.route");
 const roomRoute = require("./room.route")
 const facilityRoute = require("./facilities.route")
 const userRoutes = require("./user.route");
+const userMiddleware = require("../../middlewares/client/user.middleware");
 
 module.exports = (app) => {
+  app.use(userMiddleware.infoUser);
+
   app.use("/", homeRoute);
 
   app.use("/rooms", roomRoute);
