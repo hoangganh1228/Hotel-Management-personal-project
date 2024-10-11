fa8f7b9d78e600adf36968c80e4a0b7ef536006e
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-10-11 14:20:50 +0700

    4.Thêm ngày tạo ở quản lí người dùng

--- models/user.model.js
+++ models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "active"
     },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
     deleted: {
       type: Boolean,
       default: false,
