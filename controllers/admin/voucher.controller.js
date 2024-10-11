c41943e6e95931be5a459b879f029da289719972
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-10-11 15:24:14 +0700

    2. Làm trang voucher

--- controllers/admin/voucher.controller.js
+++ controllers/admin/voucher.controller.js
@@ -1,5 +1,17 @@
+const Voucher = require("../../models/voucher.model");
 
+const systemConfig = require("../../config/system")
 
 module.exports.index = async (req, res) => {
-  res.send("OK")
+  const find = {
+    deleted: false
+  }
+
+  const vouchers = await Voucher.find(find);
+
+  res.render("admin/pages/vouchers/index", {
+    pageTitle: "Trang voucher",
+    vouchers: vouchers
+  })
+
 }
