70f8fed4b6222843d6bef9debeae44822c729b33
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-11-06 19:59:12 +0700

    2.  Làm trang liên hệ (P2)

--- controllers/client/contact.controller.js
+++ controllers/client/contact.controller.js
@@ -1,6 +1,17 @@
+const User = require("../../models/user.model");
+
 
 module.exports.index = async (req, res) => {
+  const tokenUser = req.cookies.tokenUser;
+
+  const user = await User.findOne({
+    tokenUser: tokenUser
+  })
+
+  
+  
   res.render("client/pages/contact/index", {
     pageTitle: "Trang liên hệ",
+    user: user 
   })
 }
