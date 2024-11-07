feef294aaf2cd386f151d4285a50d54ca98ba5bf
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-11-07 16:05:45 +0700

    4. Làm trang liên hệ (P4)

--- controllers/admin/contact.controller.js
+++ controllers/admin/contact.controller.js
@@ -1,6 +1,7 @@
 const User = require("../../models/user.model");
 const Contact = require("../../models/contact.model");
 
+const sendMailHelper = require("../../helpers/sendMail");
 
 module.exports.index = async (req, res) => {
   const find = {
@@ -13,3 +14,44 @@ module.exports.index = async (req, res) => {
   })
 }
 
+module.exports.response = async (req, res) => {
+  const id = req.params.id;
+
+  const contact = await Contact.findOne({
+    _id: id
+  })
+  
+  
+  res.render("admin/pages/contact/response", {
+    pageTitle: "Phản hồi",
+    contact: contact
+  })
+}
+
+module.exports.responsePost = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const message = req.body.message
+  
+    sendMailHelper.sendMail(email, subject, message);
+    req.flash("success", "Phản hồi thành công!")  
+  } catch (error) {
+    req.flash("error", "Phản hồi thất bại!")  
+    
+  }
+  
+  res.redirect("back");
+}
+
+module.exports.delete = async (req, res) => {
+  const id = req.params.id;
+  
+  await Contact.updateOne({
+    _id: id
+  }, {
+    deleted: true
+  })
+
+  res.redirect("back")
+}
--- routes/admin/contact.route.js
+++ routes/admin/contact.route.js
@@ -5,5 +5,10 @@ const controller = require("../../controllers/admin/contact.controller");
 
 router.get("/", controller.index);
 
+router.get("/response/:id", controller.response);
+
+router.post("/response/:id", controller.responsePost);
+
+router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
