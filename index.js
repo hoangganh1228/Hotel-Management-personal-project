e34b64ec76ab5a84a73eda0c9b5655fe7d275cb0
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-11-08 15:02:58 +0700

    4. Làm trang 404

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ app.locals.moment = moment;
 route(app);
 routeAdmin(app);
 
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 })
